60a702408a1477645625938e3126fccca8b51a31
tranvansam <transamdhkh@gmail.com>
2025-11-19 18:06:45 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myapp',
+  webDir: 'dist'
+};
+
+export default config;
